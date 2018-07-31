23dd50b7e523e23e66c78a580ac3ffe4e2245b94
Indika Liyanage <indika.liyanage@ebuilder.com>
2018-07-31 10:12:44 +0530

    Some test code for ES

--- src/main/java/lk/indika/elasticsearch/MyElasticsearch.java
+++ src/main/java/lk/indika/elasticsearch/MyElasticsearch.java
@@ -1,6 +1,8 @@
 package lk.indika.elasticsearch;
 
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.elasticsearch.action.get.GetResponse;
@@ -10,6 +12,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHitField;
@@ -26,24 +29,44 @@ public class MyElasticsearch {
 
 			//set the cluster name if you use one different than "elasticsearch":
 			Settings settings = Settings.settingsBuilder()
-			        .put("cluster.name", "prodcat_inli-dev").build();
+			        .put("cluster.name", "prodcat-test").build();
 			Client client = TransportClient.builder().settings(settings).build()
-					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.26.72.121"), 9300));
 	        
 			//Add transport addresses and do something with the client...
 			//GetResponse response = client.prepareGet("products", "product", "126678").get();
 			long start = System.currentTimeMillis();
-			int page_no = 21;
+			int page_no = 1;
 			
-			SearchResponse response = client.prepareSearch("ehav-orderable-products")
-			        .setTypes("tradeitem")
-			        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-			        .setQuery(QueryBuilders.termQuery("catnode.category", "dryck"))                 // Query
+            String regex = "(^?|.*\\|)(" + "433510" + "):.*";
+            BoolQueryBuilder query = QueryBuilders.boolQuery();
+			//query.must(QueryBuilders.regexpQuery("assortments", regex));
+            //query.must(QueryBuilders.queryStringQuery("577*").field("supplierprodcode"));
+            //query.must(QueryBuilders.wildcardQuery("supplierprodcode", "*577*"));
+            //query.must(QueryBuilders.queryStringQuery("*2426542*").field("navigationnodepath"));
+            //query.must(QueryBuilders.existsQuery("replacesitemdid"));
+
+            Long[] arr = new Long[]{107468L,103908L,107521L,24674L,25430L,25543L,25555L,25696L,25831L,25833L,107555L,107592L,107673L,107723L,107747L,107794L,24592L,24598L,24697L,24811L};
+            List<Long> dids = Arrays.asList(arr);
+            BoolQueryBuilder commodityDids = QueryBuilders.boolQuery();
+			commodityDids.minimumNumberShouldMatch(1);
+
+			for (Long did : dids) {
+				commodityDids.should(QueryBuilders.termQuery("commoditydid", did));
+			}
+			query.must(commodityDids);
+            
+            query.must(QueryBuilders.termQuery("pricelistactuality", 2));
+            
+			SearchResponse response = client.prepareSearch("ehav-qa")
+			        .setTypes("products")
+			        //.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+			        .setQuery(query)                 // Query
 			        //.setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(18))     // Filter
-			        .addSort("catnode.navigationnodeid", SortOrder.ASC).addSort("tradeitemdid", SortOrder.DESC)
-			        .setFrom(page_no)
+			        //.addSort("tradeitemdid", SortOrder.DESC)
+			        //.setFrom(page_no)
 			        .setSize(20)
-			        .setExplain(true)
+			        //.setExplain(true)
 			        .execute()
 			        .actionGet();
 			
@@ -52,17 +75,25 @@ public class MyElasticsearch {
 			int i = page_no;
 			System.out.println("Total Records: " + total_recs + "\n");
 			
+            String commDids = "1234";
 			for (SearchHit hit : shits) {
-				System.out.println("Record " + i++ + " of " + total_recs + " [Product Did: " + hit.getId() + "]");
+				//System.out.println("Record " + i++ + " of " + total_recs + " [Product Did: " + hit.getId() + "]");
 				
 				Map<String, Object> fields = hit.getSource();
-				for (String key : fields.keySet()) {
-					System.out.println(key + ": " + fields.get(key));
-				}
-				System.out.println("\n----------------------------------------------------------\n ");
+                commDids += "," + fields.get("tradeitemdid") + "L";
+                System.out.print("tradeitemdid: " + fields.get("tradeitemdid"));
+                System.out.print(", " + "supplierprodcode: " + fields.get("supplierprodcode"));
+                System.out.print(", " + "prodname: " + fields.get("prodname"));
+                System.out.print(", " + "supplieragreementno: " + fields.get("supplieragreementno"));
+                System.out.println(", " + "buyerprodcode: " + fields.get("buyerprodcode"));
+                System.out.println("navigationnodepath: " + fields.get("navigationnodepath"));
+//				for (String key : fields.keySet()) {
+//					System.out.println(key + ": " + fields.get(key));
+//				}
+				//System.out.println("\n----------------------------------------------------------\n ");
 			}
 			System.out.println("Total time taken: " + (System.currentTimeMillis() - start) + "ms");
-			//System.out.println("Product Did: " + hits[0].getId());
+			System.out.println("Commodity Dids: " + commDids);
 			
 			// on shutdown
 			client.close();
--- src/main/java/lk/indika/elasticsearch/UpdateDocument.java
+++ src/main/java/lk/indika/elasticsearch/UpdateDocument.java
@@ -2,9 +2,13 @@ package lk.indika.elasticsearch;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.Map;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
@@ -16,23 +20,24 @@ public class UpdateDocument {
 
 	public static void main(String[] args) {
 		try {
-			String itemDid = "108076";
+			String itemDid = "2426229320";
 			// set the cluster name if you use one different than "elasticsearch":
-			Settings settings = Settings.settingsBuilder().put("cluster.name", "prodcat_inli-dev").build();
+			Settings settings = Settings.settingsBuilder().put("cluster.name", "prodcat-dev").build();
 			Client client = TransportClient.builder().settings(settings).build()
 					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
 			
-			UpdateRequest updateRequest = new UpdateRequest("prodcat", "item", itemDid)
+			UpdateRequest updateRequest = new UpdateRequest("ehav-test", "products", itemDid)
 			        .doc(jsonBuilder()
 			            .startObject()
-			                .field("narcoticscode", "ANZ")
-			                .field("assortmentid", "1234567")
+			                //.field("assortmentdids", "2433509,2433510,2433511,2433512,2433513")
+                            .field("authorizerdids", "26793,29320,29321,29244,29322")
+			                //.field("assortmentid", "1234567")
 			            .endObject());
 			client.update(updateRequest).get();
 			System.out.println("Updated the document with ID: " + itemDid);
 			
 			//get the updated item to test
-			GetResponse get = client.prepareGet("prodcat", "item", itemDid).get();
+			GetResponse get = client.prepareGet("ehav-test", "products", itemDid).get();
 			Map<String, Object> fields = get.getSource();
 			for (String key : fields.keySet()) {
 				System.out.println(key + ": " + fields.get(key));
@@ -46,4 +51,57 @@ public class UpdateDocument {
 
 	}
 
+    private void createUpdateDocuments() throws Exception {
+        Settings settings = Settings.settingsBuilder().put("cluster.name", "prodcat-dev").build();
+		Client client = TransportClient.builder().settings(settings).build()
+					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+		
+        //client.prepareIndex("details", "<type_name>", "<id>").setSource(putJsonDocumentString(Key, Value)).execute().actionGet();
+        
+        String json = "{" +
+            "\"user\":\"kimchy\"," +
+            "\"postDate\":\"2013-01-30\"," +
+            "\"message\":\"trying out Elasticsearch\"" +
+            "}";
+
+        IndexResponse response = client.prepareIndex("twitter", "tweet")
+                    .setSource(json)
+                    .get();
+        
+        
+    }
+    
+    private void createUpdateBulk() throws Exception {
+        Settings settings = Settings.settingsBuilder().put("cluster.name", "prodcat-dev").build();
+        Client client = TransportClient.builder().settings(settings).build()
+            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+        // either use client#prepare, or use Requests# to directly build index/delete requests
+        bulkRequest.add(client.prepareIndex("twitter", "tweet", "1")
+            .setSource(jsonBuilder()
+                .startObject()
+                .field("user", "kimchy")
+                .field("postDate", new Date())
+                .field("message", "trying out Elasticsearch")
+                .endObject()
+            )
+        );
+
+        bulkRequest.add(client.prepareIndex("twitter", "tweet", "2")
+            .setSource(jsonBuilder()
+                .startObject()
+                .field("user", "kimchy")
+                .field("postDate", new Date())
+                .field("message", "another post")
+                .endObject()
+            )
+        );
+
+        BulkResponse bulkResponse = bulkRequest.get();
+        if (bulkResponse.hasFailures()) {
+            // process failures by iterating through each bulk response item
+        }
+    }
 }
